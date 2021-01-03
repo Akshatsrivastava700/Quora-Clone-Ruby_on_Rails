b43a79d2486faf0994278ca3af6b5acd6f0416ba
Akshat Srivastava <Akshat Srivastava>
2021-01-03 20:19:48 +0530

    SignUp Page Updated

--- app/javascript/packs/signup_password.js
+++ app/javascript/packs/signup_password.js
@@ -8,3 +8,14 @@ else{
   signup_btn.style.opacity = '0.5';
 }
 });
+
+var login_input = document.getElementById('login_user_password');
+login_input.addEventListener('keyup',function(){
+var signup_btn = document.getElementById('login-btn');
+if (login_input.value.length >= 6){
+  signup_btn.style.opacity = '1';
+}
+else{
+  signup_btn.style.opacity = '0.5';
+}
+});
